e7f17ffad9de97a6ddd6e0d74c03b2a10c474dd7
Valentin Delporte <delporte.valentin@gmail.com>
2015-11-19 21:06:18 +0100

    2d first pass particle filter

---
+++ Assets/Scripts/Algo.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public struct Part
+{
+    public ParticleSystem.Particle P;
+    public float Weight;
+}
+
+public class Algo
+{
+    private static float e = 2.718281828459045f;
+    private static float sigma2 = Mathf.Pow(0.9f, 2);
+
+    public ParticleSystem Particles;
+    public Drone drone;
+    private Part[] parts;
+    public float Filtering;
+
+    public float WGauss(float a, float b)
+    {
+        float error = a - b;
+        return Mathf.Pow(e, -Mathf.Pow(error, 2) / (2 * sigma2));
+    }
+
+    public void Pass()
+    {
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[Particles.maxParticles];
+        int amount = Particles.GetParticles(particles);
+        parts = new Part[amount];
+
+        float avg = 0.0f;
+        for (int i = 0; i < amount; ++i)
+        {
+            Part part = new Part();
+            part.P = particles[i];
+
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(part.P.position, -drone.transform.right);
+            if (hit.collider != null && drone.left > 0.0f)
+            {
+                part.Weight += WGauss(drone.left, hit.distance);
+            }
+            hit = Physics2D.Raycast(part.P.position, drone.transform.right);
+            if (hit.collider != null && drone.right > 0.0f)
+            {
+                part.Weight += WGauss(drone.right, hit.distance);
+            }
+            hit = Physics2D.Raycast(part.P.position, drone.transform.up);
+            if (hit.collider != null && drone.up > 0.0f)
+            {
+                part.Weight += WGauss(drone.up, hit.distance);
+            }
+            hit = Physics2D.Raycast(part.P.position, -drone.transform.up);
+            if (hit.collider != null && drone.down > 0.0f)
+            {
+                part.Weight += WGauss(drone.down, hit.distance);
+            }
+
+            parts[i] = part;
+
+            avg += part.Weight;
+            particles[i].size = part.Weight / 10.0f;
+        }
+        avg /= amount;
+
+        float filter = avg * Filtering;
+        for (int i = 0; i < amount; ++i)
+        {
+            if (parts[i].Weight >= filter)
+            {
+                particles[i].color = Color.red;
+            }
+            else
+            {
+                particles[i].color = Color.white;
+            }
+        }
+        Particles.SetParticles(particles, amount);
+    }
+}
---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drone : MonoBehaviour
+{
+    public ParticleSystem System;
+    public float Distance = 2.0f;
+    public float left;
+    public float right;
+    public float up;
+    public float down;
+    public float Filtering = 2.0f;
+
+    private Algo algo;
+
+    void Start()
+    {
+        algo = new Algo();
+        algo.drone = this;
+        algo.Particles = System;
+    }
+
+    void Update()
+    {
+        Sensor();
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        algo.Filtering = Filtering;
+            algo.Pass();
+        //}
+    }
+
+    private void Sensor()
+    {
+        RaycastHit2D cleft = Physics2D.Raycast(transform.position, -transform.right, Distance);
+        RaycastHit2D cright = Physics2D.Raycast(transform.position, transform.right, Distance);
+        RaycastHit2D cup = Physics2D.Raycast(transform.position, transform.up, Distance);
+        RaycastHit2D cdown = Physics2D.Raycast(transform.position, -transform.up, Distance);
+
+        if (cleft.collider != null)
+        {
+            left = cleft.distance;
+            Debug.DrawLine(transform.position, cleft.point, Color.red);
+        }
+        else
+        {
+            left = -1.0f;
+        }
+        if (cright.collider != null)
+        {
+            right = cright.distance;
+            Debug.DrawLine(transform.position, cright.point, Color.red);
+        }
+        else
+        {
+            right = -1.0f;
+        }
+        if (cup.collider != null)
+        {
+            up = cup.distance;
+            Debug.DrawLine(transform.position, cup.point, Color.red);
+        }
+        else
+        {
+            up = -1.0f;
+        }
+        if (cdown.collider != null)
+        {
+            down = cdown.distance;
+            Debug.DrawLine(transform.position, cdown.point, Color.red);
+        }
+        else
+        {
+            down = -1.0f;
+        }
+    }
+}
