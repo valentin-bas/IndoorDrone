82b4d0ae20bc2f0eb37f5833f46e8718e3e5bb77
Valentin Delporte <delporte.valentin@gmail.com>
2015-11-20 15:37:12 +0100

    Version

--- Assets/Scripts/Algo.cs
+++ Assets/Scripts/Algo.cs
@@ -9,6 +9,12 @@ public struct Part
     public float Weight;
 }
 
+public struct ReusableParticle
+{
+    public ParticleSystem.Particle Particle;
+    public int Index;
+}
+
 public class Algo
 {
     private static float e = 2.718281828459045f;
@@ -18,6 +24,12 @@ public class Algo
     public Drone drone;
     private Part[] parts;
     public float Filtering;
+    public LayerMask Mask;
+    public Vector3 RelMove;
+    public int ParticleCount;
+
+    private List<ReusableParticle> reuse = new List<ReusableParticle>();
+    private List<ReusableParticle> best = new List<ReusableParticle>();
 
     public float WGauss(float a, float b)
     {
@@ -25,37 +37,119 @@ public class Algo
         return Mathf.Pow(e, -Mathf.Pow(error, 2) / (2 * sigma2));
     }
 
-    public void Pass()
+    public void SetZ(float z)
     {
         ParticleSystem.Particle[] particles = new ParticleSystem.Particle[Particles.maxParticles];
         int amount = Particles.GetParticles(particles);
+        for (int i = 0; i < amount; ++i)
+        {
+            particles[i].position = new Vector3(particles[i].position.x, particles[i].position.y, z);
+        }
+        Particles.SetParticles(particles, amount);
+    }
+
+    private void SpawnMissingParticle()
+    {
+        if (Particles.particleCount == 0)
+        {
+            Particles.Emit(ParticleCount);
+        }
+        else
+        {
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[Particles.maxParticles];
+            int amount = Particles.GetParticles(particles);
+
+            List<ParticleSystem.Particle> r = new List<ParticleSystem.Particle>();
+            List<ParticleSystem.Particle> b = new List<ParticleSystem.Particle>();
+
+            for (int i = 0; i < best.Count; ++i )
+            {
+                b.Add(best[i].Particle);
+            }
+
+            for (int i = 0; i < reuse.Count; ++i)
+            {
+                ReusableParticle p2 = best[Random.Range(0, best.Count - 1)];
+                var v = reuse[i];
+                v.Particle.position = new Vector2(p2.Particle.position.x + Random.Range(-0.2f, 0.2f), p2.Particle.position.y + Random.Range(-0.2f, 0.2f));
+                reuse[i] = v;
+                r.Add(reuse[i].Particle);
+            }
+
+            //int particleUnit = amount / reuse.Count;
+            //for (int i = 0; i < amount; ++i)
+            //{
+            //    for (int j = 0; j < particleUnit; ++j)
+            //    {
+            //        reuse[i * particleUnit + j].Particle.position 
+            //        //ParticleSystem.Particle p = new ParticleSystem.Particle();
+            //        //p.position = new Vector2(particles[i].position.x + Random.Range(-1.0f, 1.0f), particles[i].position.y + Random.Range(-1.0f, 1.0f));
+            //        //p.lifetime = 500000;
+            //        //p.size = 0.1f;
+            //        //p.color = Color.white;
+            //        //p.velocity = Vector3.zero;
+            //    }
+            //}
+
+            var arr1 = b.ToArray();
+            var arr2 = r.ToArray();
+            ParticleSystem.Particle[] newParts = new ParticleSystem.Particle[Particles.maxParticles];
+            Debug.Log(arr1.Length + " " + arr2.Length + " " + newParts.Length);
+            System.Array.Copy(arr1, 0, newParts, 0, arr1.Length);
+            System.Array.Copy(arr2, 0, newParts, arr1.Length, arr2.Length);
+
+            Particles.SetParticles(newParts, arr1.Length + arr2.Length);
+
+            best.Clear();
+            reuse.Clear();
+
+            //int diff = ParticleCount - Particles.particleCount;
+            //if (diff > 0)
+            //{
+            //    Particles.Emit(diff);
+            //}
+        }
+    }
+
+    public void Pass()
+    {
+        SpawnMissingParticle();
+
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[Particles.maxParticles];
+        int amount = Particles.GetParticles(particles);
         parts = new Part[amount];
+        Debug.Log(amount);
 
         float avg = 0.0f;
         for (int i = 0; i < amount; ++i)
         {
             Part part = new Part();
             part.P = particles[i];
+            particles[i].position += RelMove;
 
             RaycastHit2D hit;
-            hit = Physics2D.Raycast(part.P.position, -drone.transform.right);
+            hit = Physics2D.Raycast(part.P.position, drone.transform.right, Mask);
             if (hit.collider != null && drone.left > 0.0f)
             {
+                //Debug.DrawLine(part.P.position, hit.point);
                 part.Weight += WGauss(drone.left, hit.distance);
             }
-            hit = Physics2D.Raycast(part.P.position, drone.transform.right);
+            hit = Physics2D.Raycast(part.P.position, -drone.transform.right, Mask);
             if (hit.collider != null && drone.right > 0.0f)
             {
+                //Debug.DrawLine(part.P.position, hit.point);
                 part.Weight += WGauss(drone.right, hit.distance);
             }
-            hit = Physics2D.Raycast(part.P.position, drone.transform.up);
+            hit = Physics2D.Raycast(part.P.position, -drone.transform.up, Mask);
             if (hit.collider != null && drone.up > 0.0f)
             {
+                //Debug.DrawLine(part.P.position, hit.point);
                 part.Weight += WGauss(drone.up, hit.distance);
             }
-            hit = Physics2D.Raycast(part.P.position, -drone.transform.up);
+            hit = Physics2D.Raycast(part.P.position, drone.transform.up, Mask);
             if (hit.collider != null && drone.down > 0.0f)
             {
+                //Debug.DrawLine(part.P.position, hit.point);
                 part.Weight += WGauss(drone.down, hit.distance);
             }
 
@@ -66,18 +160,24 @@ public class Algo
         }
         avg /= amount;
 
-        float filter = avg * Filtering;
+
+        float filter = avg;
         for (int i = 0; i < amount; ++i)
         {
             if (parts[i].Weight >= filter)
             {
                 particles[i].color = Color.red;
+                best.Add(new ReusableParticle { Particle = particles[i], Index = i });
+                //newParticles.Add(particles[i]);
             }
             else
             {
-                particles[i].color = Color.white;
+                //particles[i].color = Color.black;
+                particles[i].color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+                reuse.Add(new ReusableParticle { Particle = particles[i], Index = i });
             }
         }
         Particles.SetParticles(particles, amount);
+        //Particles.SetParticles(particles, amount);
     }
 }
--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -10,37 +10,89 @@ public class Drone : MonoBehaviour
     public float up;
     public float down;
     public float Filtering = 2.0f;
+    public float MoveSpeed = 5.0f;
+    public LayerMask Mask;
+    public int ParticleCount = 2000;
+    public float Noise = 0.1f;
 
     private Algo algo;
 
+    private new Rigidbody2D rigidbody;
+    private Vector3 oldPosition;
+
     void Start()
     {
+        oldPosition = transform.position;
+
         algo = new Algo();
         algo.drone = this;
         algo.Particles = System;
+        algo.SetZ(0.0f);
+
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(Pass());
     }
 
     void Update()
     {
+        DoInput();
         Sensor();
 
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
-        algo.Filtering = Filtering;
-            algo.Pass();
+        //    Vector3 relMove = transform.position - oldPosition;
+        //    algo.Filtering = Filtering;
+        //    algo.Mask = Mask;
+        //    algo.RelMove = relMove;
+        //    algo.ParticleCount = ParticleCount;
+        //    algo.Pass();
+
+        //    oldPosition = transform.position;
         //}
+    }
+
+    private IEnumerator Pass()
+    {
+        while (true)
+        {
+            Vector3 relMove = transform.position - oldPosition;
+            Debug.Log(relMove);
+
+            algo.Filtering = Filtering;
+            algo.Mask = Mask;
+            algo.RelMove = relMove;
+            algo.ParticleCount = ParticleCount;
+            algo.Pass();
+
+            oldPosition = transform.position;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void DoInput()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 moveDirection = new Vector2(0.0f, MoveSpeed * Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection) * Time.deltaTime * 10.0f;
+        rigidbody.velocity = moveDirection;
+
+        transform.Rotate(transform.forward * -horizontal * Time.deltaTime * 360.0f);
     }
 
     private void Sensor()
     {
-        RaycastHit2D cleft = Physics2D.Raycast(transform.position, -transform.right, Distance);
-        RaycastHit2D cright = Physics2D.Raycast(transform.position, transform.right, Distance);
-        RaycastHit2D cup = Physics2D.Raycast(transform.position, transform.up, Distance);
-        RaycastHit2D cdown = Physics2D.Raycast(transform.position, -transform.up, Distance);
+        RaycastHit2D cleft = Physics2D.Raycast(transform.position, -transform.right, Distance, Mask);
+        RaycastHit2D cright = Physics2D.Raycast(transform.position, transform.right, Distance, Mask);
+        RaycastHit2D cup = Physics2D.Raycast(transform.position, transform.up, Distance, Mask);
+        RaycastHit2D cdown = Physics2D.Raycast(transform.position, -transform.up, Distance, Mask);
 
         if (cleft.collider != null)
         {
-            left = cleft.distance;
+            left = cleft.distance + Random.Range(-Noise, Noise);
             Debug.DrawLine(transform.position, cleft.point, Color.red);
         }
         else
@@ -49,7 +101,7 @@ public class Drone : MonoBehaviour
         }
         if (cright.collider != null)
         {
-            right = cright.distance;
+            right = cright.distance + Random.Range(-Noise, Noise);
             Debug.DrawLine(transform.position, cright.point, Color.red);
         }
         else
@@ -58,7 +110,7 @@ public class Drone : MonoBehaviour
         }
         if (cup.collider != null)
         {
-            up = cup.distance;
+            up = cup.distance + Random.Range(-Noise, Noise);
             Debug.DrawLine(transform.position, cup.point, Color.red);
         }
         else
@@ -67,7 +119,7 @@ public class Drone : MonoBehaviour
         }
         if (cdown.collider != null)
         {
-            down = cdown.distance;
+            down = cdown.distance + Random.Range(-Noise, Noise);
             Debug.DrawLine(transform.position, cdown.point, Color.red);
         }
         else
